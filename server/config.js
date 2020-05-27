b29a9139917ef972964b97c202d534956c5af6ae
Vishwa Krishnakumar <vishwa306@gmail.com>
2020-05-28 00:21:51 +0530

    Config changes
    Changed Database url

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/university"
+    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraet@localhost/university"
 };
